0e6d33d6f16557a6816027503ff4a6e9e2e64673
Hanss <hnm@agroclimatica.com>
2024-03-07 09:56:27 -0600

    Se agregan los repositorios para interactuar con la BD

---
+++ NetForeMostTest/WinFormsNetForeMostTest/DataAccess/Repository/CompraRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace WinFormsNetForeMostTest.DataAccess.Repository
+{
+    public class CompraRepository
+    {
+        private readonly string _connectionString;
+
+        public CompraRepository()
+        {
+            _connectionString = AppConfig.ConnectionString;
+        }
+
+        public void RegistrarCompra(int productoID, int cantidadComprada, decimal precioUnitario)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO Compras (ProductoID, CantidadComprada, PrecioUnitario, FechaCompra) " +
+                               "VALUES (@ProductoID, @CantidadComprada, @PrecioUnitario, @FechaCompra)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ProductoID", productoID);
+                command.Parameters.AddWithValue("@CantidadComprada", cantidadComprada);
+                command.Parameters.AddWithValue("@PrecioUnitario", precioUnitario);
+                command.Parameters.AddWithValue("@FechaCompra", DateTime.Now);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ NetForeMostTest/WinFormsNetForeMostTest/DataAccess/Repository/VentaRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using WinFormsNetForeMostTest.DataAcess.Utils;
+
+namespace WinFormsNetForeMostTest.DataAccess.Repository
+{
+    public class VentaRepository
+    {
+        private readonly string _connectionString;
+
+        public VentaRepository()
+        {
+            _connectionString = AppConfig.ConnectionString;
+        }
+
+        public void RegistrarVenta(int productoID, int cantidadVendida)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "INSERT INTO Ventas (ProductoID, CantidadVendida, FechaVenta) VALUES (@ProductoID, @CantidadVendida, @FechaVenta)";
+                        command.Parameters.AddWithValue("@ProductoID", productoID);
+                        command.Parameters.AddWithValue("@CantidadVendida", cantidadVendida);
+                        command.Parameters.AddWithValue("@FechaVenta", DateTime.Now);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier error y mostrar un mensaje al usuario
+                Console.WriteLine($"Error al registrar la venta: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
